7d364d36aaf74aa28f7ada9ce3490fbc86270cbc
JaviAzagra <javiazagra12@gmail.com>
2022-11-03 18:06:43 +0100

    FIX: fix an image

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 
 
 function lanzardado() {
+
     let aleatorio = 0;
+
     aleatorio = Math.floor(Math.random()*6) +1 ;
+    
     console.log(aleatorio);
     
     if (aleatorio === 1) {
         document.getElementById("dado").style.backgroundImage = "url(./assets/dado1.png)" ;
+
     } else if (aleatorio === 2){
+
         document.getElementById("dado").style.backgroundImage = "url(./assets/dado2.png)" ;
+
     } else if (aleatorio === 3){
+
         document.getElementById("dado").style.backgroundImage = "url(./assets/dado3.png)" ;
+
     } else if (aleatorio === 4){
+
         document.getElementById("dado").style.backgroundImage = "url(./assets/dado4.png)" ;
+
     } else if (aleatorio === 5){
+
         document.getElementById("dado").style.backgroundImage = "url(./assets/dado5.png)" ;
+
     } else if (aleatorio === 6){
+
         document.getElementById("dado").style.backgroundImage = "url(./assets/dado6.png)" ;
+
     } 
 }
 
